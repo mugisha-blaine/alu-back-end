872583ed3c991c0dacc9263f093cb3d68fb642ec
root <mugisha-blaine>
2023-07-06 09:45:41 +0000

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-"""script that returns information about his/her TODO list progress. """
+""" Script that uses JSONPlaceholder API to get information about employee """
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-
-    # Make a GET request to the API endpoint
-    response = requests.get(url)
+    
+    def get_employee_todo_progress(employee_id):
+    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
 
     if response.status_code == 200:
         todos = response.json()
 
-        
         completed_tasks = [todo for todo in todos if todo['completed']]
 
         employee_name = todos[0]['username']
         number_of_done_tasks = len(completed_tasks)
         total_number_of_tasks = len(todos)
 
-        # Display progress information
         print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks}):")
 
-        # Display completed tasks
         for task in completed_tasks:
             print(f"\t{task['title']}")
 
     else:
         print(f"Failed to retrieve TODO list for employee {employee_id}.")
 
-# Provide the employee ID as a command-line argument
 employee_id = int(input("Enter the employee ID: "))
 get_employee_todo_progress(employee_id)
