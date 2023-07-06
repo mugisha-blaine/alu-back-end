6c447fe0dc64c322560ba3b8c48e1a0fb48f66fe
root <mugisha-blaine>
2023-07-06 11:19:23 +0000

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Script that uses JSONPlaceholder API to get information about employee """
 import requests
+""" Script that uses JSONPlaceholder API to get information about employee """
 
 
 if __name__ == "__main__":
