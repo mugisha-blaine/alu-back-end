79a652851c0263e4011704430bb68463767f3873
root <mugisha-blaine>
2023-07-06 12:17:02 +0000

    task 0

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,4 +7,37 @@ import sys
 
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'    
+    def export_employee_todo_csv(employee_id):
+    """Make a GET request to the API endpoint"""
+    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+
+    if response.status_code == 200:
+        todos = response.json()
+
+ 
+        task_data = []
+        for task in todos:
+            task_data.append([
+                task['userId'],
+                task['username'],
+                str(task['completed']),
+                task['title']
+            ])
+
+        
+        file_name = f"{employee_id}.csv"
+
+       
+        with open(file_name, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+            writer.writerows(task_data)
+
+        print(f"TODO list exported to {file_name} successfully.")
+
+    else:
+        print(f"Failed to retrieve TODO list for employee {employee_id}.")
+
+
+employee_id = int(input("Enter the employee ID: "))
+export_employee_todo_csv(employee_id)
