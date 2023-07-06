aaa8a1c63c5d1afa4617cac134d3cdcadb5a8889
root <mugisha-blaine>
2023-07-06 09:05:44 +0000

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+
+def get_employee_todo_progress(employee_id):
+    # Make a GET request to the API endpoint
+    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+
+    if response.status_code == 200:
+        todos = response.json()
+
+        completed = [todo for todo in todos if todo['completed']]
+
+        employee_name = todos[0]['username']
+        number_of_done_tasks = len(completed_tasks)
+        total_number_of_tasks = len(todos)
+
+        # Display progress information
+        print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks}):")
+
+        # Display completed tasks
+        for task in completed_tasks:
+            print(f"\t{task['title']}")
+
+    else:
+        print(f"Failed to retrieve TODO list for employee {employee_id}.")
+
+employee_id = int(input("Enter the employee ID: "))
+get_employee_todo_progress(employee_id)
+
---
+++ api/README.md
@@ -0,0 +1,3 @@
+Basic and advanced API using Python: You will learn how to design, implement, and 
+consume APIs using Python. This includes an introduction to the principles of RESTful
+ API design, as well as best practices for building scalable and maintainable API architectures.
