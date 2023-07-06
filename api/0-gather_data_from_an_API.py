09e052e5426fb229ce716a45539e311ec0df7597
root <mugisha-blaine>
2023-07-06 10:06:14 +0000

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,29 +4,24 @@ import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    """
-    Retrieves information about an employee's TODO list progress from the REST API
-    """
-    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
 
     if response.status_code == 200:
         todos = response.json()
 
         completed_tasks = [todo for todo in todos if todo['completed']]
 
+        # Extract relevant information
         employee_name = todos[0]['username']
         number_of_done_tasks = len(completed_tasks)
         total_number_of_tasks = len(todos)
 
+        
         print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks}):")
 
-        # Display completed tasks
         for task in completed_tasks:
             print(f"\t{task['title']}")
 
     else:
         print(f"Failed to retrieve TODO list for employee {employee_id}.")
-
-employee_id = int(input("Enter the employee ID: "))
-get_employee_todo_progress(employee_id)
+if __name__ == "__main__":
