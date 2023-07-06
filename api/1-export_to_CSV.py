2ac26e428342e4fa81855474238b7550a5d708a1
root <mugisha-blaine>
2023-07-06 12:13:19 +0000

    task 0

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,11 +2,9 @@
 """ Script that uses JSONPlaceholder API to get information about employee """
 import csv
 import json
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-
-    
+    url = 'https://jsonplaceholder.typicode.com/'    
