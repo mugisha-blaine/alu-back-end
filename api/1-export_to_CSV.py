c33641dc29fb0813225d7036b8b004dc7a11b65c
root <mugisha-blaine>
2023-07-09 20:51:02 +0000

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
-"""a script tha use api to get information of employee"""
+""" Script that uses API to get information about employee """
 import csv
 import requests
 import sys
 
 
 if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
-    name = user.json().get('username')
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    userid = sys.argv[1]
+    employee = '{}users/{}'.format(url, userid)
+    response = requests.get(employee)
+    nom = response.json()
+    name = nom.get('username')
 
-    filename = userId + '.csv'
-    with open(filename, mode='w') as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"',
-                            quoting=csv.QUOTE_ALL, lineterminator='\n')
-        for task in todos.json():
-            if task.get('userId') == int(userId):
-                writer.writerow([userId, name, str(task.get('completed')),
-                                 task.get('title')])
+    todos = '{}todos?userId={}'.format(url, userid)
+    response = requests.get(todos)
+    tasks = response.json()
+    done = []
+    for task in tasks:
+        done.append([userid,
+                     name,
+                     task.get('completed'),
+                     task.get('title')])
+
+    filename = '{}.csv'.format(userid)
+    with open(filename, mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file,
+                                     delimiter=',',
+                                     quotechar='"',
+                                     quoting=csv.QUOTE_ALL)
+        for task in done:
+            employee_writer.writerow(task)i
